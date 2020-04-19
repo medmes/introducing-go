f99c9a115186911dd03288babdd5b119daac8c12
medmes <mohammed.mesaoudi@gmail.com>
2020-04-19 18:01:54 +0000

    Chapter 8 - Packages - Solving Package chapter Exercises!

--- src/golang-book/chapter8/main.go
+++ src/golang-book/chapter8/main.go
@@ -10,4 +10,18 @@ func main() {
 	xs := []float64{1, 2, 3, 4}
 	avg := m.Average(xs)
 	fmt.Println(avg)
+
+	// Exercises: Min, Max
+	min := m.Min(xs)
+	fmt.Println(min)
+
+	max := m.Max(xs)
+	fmt.Println(max)
+
+	// How would you document the functions you created in #4?
+	// By commenting the function Min, Max and generate the documentation using:
+	// godoc golang-book/chapter8/math Min
+	// godoc golang-book/chapter8/math Max
+	// After generation,  the documentation is also available in web form by running this command:
+	// godoc -http=":6060"
 }
--- src/golang-book/chapter8/math/math.go
+++ src/golang-book/chapter8/math/math.go
@@ -11,3 +11,25 @@ func Average(xs []float64) float64 {
 	}
 	return total / float64(len(xs))
 }
+
+// Calculate min of numbers list:
+func Min(numbers []float64) (min float64) {
+	min = numbers[0]
+	for _, v := range numbers {
+		if v < min {
+			min = v
+		}
+	}
+	return
+}
+
+// Calculate max of numbers list:
+func Max(numbers []float64) (max float64) {
+	max = numbers[0]
+	for _, v := range numbers {
+		if v > max {
+			max = v
+		}
+	}
+	return
+}
